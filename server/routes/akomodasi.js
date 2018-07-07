27f734b560a98d5be88bd2f1dd907e2cab90848b
Prana Jaya Asmara Pratama <pranajaya.pratama@gmail.com>
2018-07-07 22:00:44 +0700

    Update akomodasi.js

--- server/routes/akomodasi.js
+++ server/routes/akomodasi.js
@@ -17,4 +17,6 @@ router.post('/cari', AkomodasiController.searchAkomodasi);
 
 router.post('/carisektor', AkomodasiController.searchSektor);
 
+router.post('/carikloter', AkomodasiController.searchKloter);
+
 module.exports = router;
