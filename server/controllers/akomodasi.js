37f9c5e05239c5f3490cebccfc6b3c9be8be7fe0
Prana Jaya Asmara Pratama <pranajaya.pratama@gmail.com>
2018-07-07 21:47:29 +0700

    Update akomodasi.js

--- server/controllers/akomodasi.js
+++ server/controllers/akomodasi.js
@@ -98,6 +98,24 @@ class Akomodasi {
       res.send(err)
     })
   }
+  
+  static searchSektor(req, res) {
+    AkomodasiModel.find().where('lokasi').equals(req.body.lokasi).
+    where('sektor').equals(req.body.sektor)
+    .then(dataAkomodasi => {
+      if (dataAkomodasi.length == 0) {
+        res.send('Data Tidak Di Temukan atau Data Yang Anda Masukan Salah!!')
+      } else {
+        res.send({
+          Message: 'Data Berhasil Di Temukan',
+          Data: dataAkomodasi
+        })
+      }
+    })
+    .catch(err => {
+      res.send(err)
+    })
+  }
 
 }
 
